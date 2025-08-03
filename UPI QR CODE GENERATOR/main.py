69968d6ebb3a6c22669f234d9916485a58bcb7c3
Darkness-pyth <cubfun36@gmail.com>
2025-08-03 21:23:50 +0530

    Update main.py

--- UPI QR CODE GENERATOR/main.py
+++ UPI QR CODE GENERATOR/main.py
@@ -1,11 +1,13 @@
+# pip install qrcode module to use it  
 import qrcode
 
-upi_id = input("Enter Your UPI ID: ")
+upi_id = input("Enter Your UPI ID: ") # Take input from users for their Upi id
 
-qr_code_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name'
 
-Pay_qr = qrcode.make(qr_code_url)
+qr_code_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name' #Here we Define qr code url structure
 
-Pay_qr.save('Pay_qr.png')
+Pay_qr = qrcode.make(qr_code_url) # Here We use qrcode module to make qrcode
 
-Pay_qr.show()
+Pay_qr.save('Pay_qr.png') # Here we save it in our computer as an image
+
+Pay_qr.show() # Here we show the qr using pillow module
